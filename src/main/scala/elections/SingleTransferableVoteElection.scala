cc060aec80de2ac90d3a92e498fd63a3caee0b77
Dag Arneson <sanedragon@gmail.com>
2018-04-30 23:27:47 -0700

    Get trivial STV test passing

--- src/main/scala/elections/SingleTransferableVoteElection.scala
+++ src/main/scala/elections/SingleTransferableVoteElection.scala
@@ -1,75 +1,82 @@
 package elections
 
-class SingleTransferableVoteElectionResult(rounds: List[STVRoundResult]) extends ElectionResult
+class SingleTransferableVoteElectionResult(val rounds: List[STVRoundResult]) extends ElectionResult
 
-class STVRoundResult(val firstPlaceVotes: Map[Candidate, Int], val winners: Set[Candidate], val losers: Set[Candidate])
+case class STVRoundResult(val firstPlaceVotes: Map[Candidate, Int], val winners: Set[Candidate], val losers: Set[Candidate])
 
+object SingleTransferableVoteElection {
+  val DroopQuota: (Int, Int) => Int = (numBallots, numCandidates) => (numBallots / (numCandidates + 1)) + 1
+  val HareQuota: (Int, Int) => Int = (numBallots, numCandidates) => (numBallots.toDouble / numCandidates).ceil.toInt
+}
 
-abstract class SingleTransferableVoteElection(val candidates: Set[Candidate], val numPositions: Int) extends Election[RankedBallot, SingleTransferableVoteElectionResult] {
-  val quotaFromBallotCount: Int => Int
+class SingleTransferableVoteElection(val candidates: Set[Candidate], val numPositions: Int, val quota: (Int, Int) => Int) extends Election[RankedBallot, SingleTransferableVoteElectionResult] {
 
   def countBallots(ballots: Set[RankedBallot]): SingleTransferableVoteElectionResult = {
-    val quota = quotaFromBallotCount(ballots.size)
+    val votesToGetElected = quota(ballots.size, candidates.size)
 
-    new SingleTransferableVoteElectionResult(recurseRound(ballots.map(b => WeightedRankedBallot(b,1.0,b.ranking)), quota))
+    new SingleTransferableVoteElectionResult(recurseRound(ballots.map(b => WeightedRankedBallot(b,1.0,b.ranking)), votesToGetElected, numPositions))
   }
 
   def recurseRound(
                     ballots: Set[WeightedRankedBallot],
-                    quota: Int
+                    quota: Int,
+                    numCandidatesRemaining: Int
                   ): List[STVRoundResult] = {
-    val firstPlaceVoteCountByCandidate: Map[Candidate, Int] = ballots.foldLeft(Map.empty[Candidate, Int])((tally, ballot) => {
-      val vote = ballot.remainingCandidates.head
-      tally.updated(vote, tally.getOrElse(vote, 0) + 1)
-    })
+    if (numCandidatesRemaining == 0 || ballots.isEmpty) Nil
+    else {
+      val firstPlaceVoteCountByCandidate: Map[Candidate, Int] = ballots.foldLeft(Map.empty[Candidate, Int])((tally, ballot) => {
+        val vote = ballot.remainingCandidates.head
+        tally.updated(vote, tally.getOrElse(vote, 0) + 1)
+      })
 
-    // TODO: Check diversity quotas here. Will also require the already elected candidates passed through recursion.
-    val winners: Set[Candidate] = firstPlaceVoteCountByCandidate.foldLeft(Set.empty[Candidate])((winners, p) => {
-      val (candidate, count) = p
-      if(count > quota) { // TODO: > or >= ?
-        winners + candidate
-      } else {
-        winners
-      }
-    })
-
-    val losers: Set[Candidate] = if(winners.isEmpty) {
-      val lowestVoteCount = firstPlaceVoteCountByCandidate.values.min
-      // It's not very likely with a sizeable electorate that more than one candidate loses here
-      // But to be fair, if they have the same vote count they all have to lose.
-      // This could potentially lead to a tie for last place
-      firstPlaceVoteCountByCandidate.foldLeft(Set.empty[Candidate])((losers, p) => {
+      // TODO: Check diversity quotas here. Will also require the already elected candidates passed through recursion.
+      val winners: Set[Candidate] = firstPlaceVoteCountByCandidate.foldLeft(Set.empty[Candidate])((winners, p) => {
         val (candidate, count) = p
-        if(count == lowestVoteCount) {
-          losers + candidate
+        if (count >= quota) {
+          winners + candidate
         } else {
-          losers
+          winners
         }
       })
-    } else Set.empty
 
-    val excludedCandidates = winners ++ losers
+      val losers: Set[Candidate] = if (winners.isEmpty) {
+        val lowestVoteCount = firstPlaceVoteCountByCandidate.values.min
+        // It's not very likely with a sizeable electorate that more than one candidate loses here
+        // But to be fair, if they have the same vote count they all have to lose.
+        // This could potentially lead to a tie for last place
+        firstPlaceVoteCountByCandidate.foldLeft(Set.empty[Candidate])((losers, p) => {
+          val (candidate, count) = p
+          if (count == lowestVoteCount) {
+            losers + candidate
+          } else {
+            losers
+          }
+        })
+      } else Set.empty
 
-    if(excludedCandidates.nonEmpty) {
+      val excludedCandidates = winners ++ losers
 
-      val winnerQuotaFactors: Map[Candidate, Double] = firstPlaceVoteCountByCandidate.foldLeft(Map.empty[Candidate, Double])((factors, p) => {
-        val (candidate, count) = p
-        if (winners.contains(candidate)) {
-          factors.updated(candidate, (count - quota).toDouble / count)
-        } else factors
-      })
+      if (excludedCandidates.nonEmpty) {
 
-      val newWeightedBallots: Set[WeightedRankedBallot] = ballots.map(b => {
-        WeightedRankedBallot(
-          b.originalBallot,
-          winnerQuotaFactors.getOrElse(b.remainingCandidates.head, 1.0) * b.weight,
-          b.remainingCandidates.filterNot(excludedCandidates.contains)
-        )
-      })
+        val winnerQuotaFactors: Map[Candidate, Double] = firstPlaceVoteCountByCandidate.foldLeft(Map.empty[Candidate, Double])((factors, p) => {
+          val (candidate, count) = p
+          if (winners.contains(candidate)) {
+            factors.updated(candidate, (count - quota).toDouble / count)
+          } else factors
+        })
 
-      new STVRoundResult(firstPlaceVoteCountByCandidate, winners, losers) :: recurseRound(newWeightedBallots, quota)
-    } else {
-      Nil
+        val newWeightedBallots: Set[WeightedRankedBallot] = ballots.map(b => {
+          WeightedRankedBallot(
+            b.originalBallot,
+            winnerQuotaFactors.getOrElse(b.remainingCandidates.head, 1.0) * b.weight,
+            b.remainingCandidates.filterNot(excludedCandidates.contains)
+          )
+        })
+
+        new STVRoundResult(firstPlaceVoteCountByCandidate, winners, losers) :: recurseRound(newWeightedBallots, quota, numCandidatesRemaining - 1)
+      } else {
+        Nil
+      }
     }
   }
 }
--- src/test/scala/elections/SingleTransferableVoteElectionSpec.scala
+++ src/test/scala/elections/SingleTransferableVoteElectionSpec.scala
@@ -1,10 +1,20 @@
 package elections
 
-import org.scalatest._
+import elections.SingleTransferableVoteElection.DroopQuota
 
-class SingleTransferableVoteElectionSpec extends FlatSpec with Matchers {
+class SingleTransferableVoteElectionSpec extends BaseSpec {
   "A SingleTransferableVoteElection" should "count correctly for a trivial case" in {
-    "this test" should be ("written")
+    val election = new SingleTransferableVoteElection(Set(alice, bob), 1, DroopQuota)
+
+    val ballots = Set(new RankedBallot(election, List(alice, bob)))
+
+    val result = election.countBallots(ballots)
+
+    result.rounds.size should be (1)
+    result.rounds.head.winners should be (Set(alice))
+    result.rounds.head.losers should be (Set.empty)
+    result.rounds.head.firstPlaceVotes(alice) should be (1)
+    result.rounds.head.firstPlaceVotes.get(bob) should be (None)
   }
 
   it should "count correctly for a slightly less trivial case" in {
