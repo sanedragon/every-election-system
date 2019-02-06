3ad5b338caa431408cf847f2f98720fb3ad3cdf8
Dag Arneson <sanedragon@gmail.com>
2019-02-05 23:49:35 -0800

    Trivial RRV test

--- src/test/scala/elections/ReweightedRankedVotingElectionSpec.scala
+++ src/test/scala/elections/ReweightedRankedVotingElectionSpec.scala
@@ -1,10 +1,17 @@
 package elections
 
-import org.scalatest._
-
-class ReweightedRankedVotingElectionSpec extends FlatSpec with Matchers {
+class ReweightedRankedVotingElectionSpec extends BaseSpec {
   "A ReweightedRankedVotingElection" should "count correctly for a trivial case" in {
-    "this test" should be ("written")
+    val candidates = Set(alice, bob)
+
+    val election = new ReweightedRankedVotingElection(candidates, 1)
+
+    val ballots = Set(new ScoreBallot(election, Map(alice -> 1, bob -> 0)))
+
+    val result = election.countBallots(ballots)
+
+    result.winners should be (List(alice))
+    result.rounds.head.roundResults should be (Map(alice -> 1, bob -> 0))
   }
 
   it should "count correctly for a slightly less trivial case" in {
